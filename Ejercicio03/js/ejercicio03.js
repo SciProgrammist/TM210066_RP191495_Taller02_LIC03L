6339e6edbd071181856c4e34ef347bf91c1f72a9
MariaTorres0 <mariatorres11001@gmail.com>
2022-10-10 22:08:36 -0600

    Carpetas corregidas

---
+++ Ejercicio03/js/ejercicio03.js
@@ -0,0 +1,5 @@
+//Criterio de cuado de texto
+function criterio() {
+    let message = "Taller Practico 02 - LIC";
+    window.alert(message);
+}
--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
-//Criterio de cuado de texto 
+//Criterio de cuado de texto
 function criterio() {
     let message = "Taller Practico 02 - LIC";
     window.alert(message);
 }
-//Problema 01
-
-//Problema 02
-
-//Problema 03
