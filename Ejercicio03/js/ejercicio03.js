5c9924bbed44b302c6659fca8f14919cf5c04114
SciProgrammist <RP191495@alumno.udb.edu.sv>
2022-10-12 01:50:56 -0600

    FormEjercicio03Completed

--- Ejercicio03/js/ejercicio03.js
+++ Ejercicio03/js/ejercicio03.js
@@ -20,31 +20,31 @@ let current = 1;
 nextBtnFirst.addEventListener("click", function(event){
   event.preventDefault();
   slidePage.style.marginLeft = "-25%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
+  bullet[current].classList.add("active");
+  progressCheck[current].classList.add("active");
+  progressText[current].classList.add("active");
   current += 1;
 });
 nextBtnSec.addEventListener("click", function(event){
   event.preventDefault();
   slidePage.style.marginLeft = "-50%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
+  bullet[current ].classList.add("active");
+  progressCheck[current].classList.add("active");
+  progressText[current].classList.add("active");
   current += 1;
 });
 nextBtnThird.addEventListener("click", function(event){
   event.preventDefault();
   slidePage.style.marginLeft = "-75%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
+  bullet[current].classList.add("active");
+  progressCheck[current].classList.add("active");
+  progressText[current].classList.add("active");
   current += 1;
 });
 submitBtn.addEventListener("click", function(){
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
+  bullet[current-1].classList.add("active");
+  progressCheck[current-1].classList.add("active");
+  progressText[current-1].classList.add("active");
   current += 1;
   setTimeout(function(){
     alert("Your Form Successfully Signed up");
@@ -54,24 +54,24 @@ submitBtn.addEventListener("click", function(){
 prevBtnSec.addEventListener("click",function(event) {
     event.preventDefault();
     slidePage.style.marginLeft = "0%";
-    bullet[current - 2].classList.remove("active");
-    progressCheck[current - 2].classList.remove("active");
-    progressText[current - 2].classList.remove("active");
+    bullet[current - 1].classList.remove("active");
+    progressCheck[current - 1].classList.remove("active");
+    progressText[current - 1].classList.remove("active");
     current-=1;
 });
 prevBtnThird.addEventListener("click",function(event) {
     event.preventDefault();
     slidePage.style.marginLeft = "-25%";
-    bullet[current - 2].classList.remove("active");
-    progressCheck[current - 2].classList.remove("active");
-    progressText[current - 2].classList.remove("active");
+    bullet[current - 1].classList.remove("active");
+    progressCheck[current - 1].classList.remove("active");
+    progressText[current - 1].classList.remove("active");
     current-=1;
 });
 prevBtnFourth.addEventListener("click",function(event) {
     event.preventDefault();
     slidePage.style.marginLeft = "-50%";
-    bullet[current - 2].classList.remove("active");
-    progressCheck[current - 2].classList.remove("active");
-    progressText[current - 2].classList.remove("active");
+    bullet[current - 1].classList.remove("active");
+    progressCheck[current - 1].classList.remove("active");
+    progressText[current - 1].classList.remove("active");
     current-=1;
 });
